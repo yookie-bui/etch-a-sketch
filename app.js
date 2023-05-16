d51b2c6245b7cf81c3c3f91d1865300aa6bf4097
Yookie Bui <81119866+yookie-bui@users.noreply.github.com>
2023-05-17 00:01:59 +1200

    grid size adjustable, add buttons

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const sketchBoard = document.getElementById('container');
+const grid = document.getElementById('grid');
+let gridSize = grid.value;
 
 
-for (let i = 0; i < 16; i++) {
-    var row = document.createElement('div');
-    row.className = 'row';
-    for (let j = 0; j < 16; j++) {
-        var square = document.createElement('div');
-        square.className = 'square';
-        row.appendChild(square);
+
+const createGrid = (gridSize) => {
+    for (let i = 0; i < gridSize; i++) {
+        var row = document.createElement('div');
+        let rowSize = 100/ gridSize;
+        row.setAttribute('style', `width: 100%; height: ${rowSize}%`)
+        row.className = 'row';
+        for (let j = 0; j < gridSize; j++) {
+            var square = document.createElement('div');
+            square.className = 'square';
+            let squareSize = 100 / gridSize;
+            square.setAttribute('style', `width:${squareSize}%; height:100%`);
+            row.appendChild(square);
+        }
+        sketchBoard.appendChild(row);
+    };
+}
+
+const emptyGrid = () => {
+    const row = document.getElementsByClassName('row');
+    while (row.length > 0) {
+        row[0].parentNode.removeChild(row[0]);
     }
-    sketchBoard.appendChild(row);
-};
-// sketchBoard.appendChild(row);
+}
 
-// for (let i = 0; i < 16; i++) {
-//     sketchBoard.appendChild(row)
-//     for (let j = 0; i < 16; j++) {
-//         row.appendChild(squareDiv);
-//     } 
-// }
+createGrid(gridSize);
+
+grid.addEventListener('input', ()=>{
+    emptyGrid();
+    createGrid(grid.value);
+})
